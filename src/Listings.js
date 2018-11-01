8174a03484f8bc3e4d6bbdb073f6ba1337e34026
Gautam Dhameja <gautamdhameja@users.noreply.github.com>
2018-11-02 00:52:52 +0100

    added actions on listings

--- src/Listings.js
+++ src/Listings.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import { Item, Label } from 'semantic-ui-react'
+import { Item, Label, Icon, Button } from 'semantic-ui-react'
 
 const ListingItem = ({ name, hash, challenge, isWhitelisted, owner }) => {
     return (<Item>
+        <Item.Image className='text-center' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: 50}}>
+            <Icon name={ isWhitelisted? 'check circle outline' : 'circle outline' } size='big' />
+        </Item.Image>
         <Item.Content>
             <Item.Header><b>{name}</b></Item.Header>
             <Item.Meta>
@@ -10,6 +13,11 @@ const ListingItem = ({ name, hash, challenge, isWhitelisted, owner }) => {
                 <Label>ChallengeID: <b>{challenge}</b></Label>
                 <Label>Owner: <b>{owner}</b></Label>
             </Item.Meta>
+            <Item.Extra>
+                <Button basic color='green' size='mini' floated='right'>Resolve</Button>
+                <Button basic color='blue' size='mini' floated='right'>Challenge</Button>
+                <Button basic color='grey' size='mini' floated='right'>Vote</Button>
+            </Item.Extra>
         </Item.Content>
     </Item>)
 }
